cdaa1118a5abd7282feb043927fd5f50bea3730f
IsmaelSantiagoo <ismaelfreitas.santiago@gmail.com>
2024-09-07 22:30:45 -0300

    Feature: Componente InOutComes adicionado

---
+++ src/components/InOutComes/index.tsx
@@ -0,0 +1,25 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faArrowUpLong } from "@fortawesome/free-solid-svg-icons"
+import { faArrowDownLong } from "@fortawesome/free-solid-svg-icons"
+import { InOutComesProps } from "./types"
+
+const InOutComes = ({type, value, porcentage, severity}: InOutComesProps) => {
+  return (
+    <div className="w-full flex gap-5 items-center">
+      <div className={`${severity === 'success'?'bg-sky-400':'bg-projectPallet-secondary'} w-14 h-12 p-2 rounded-xl flex items-center justify-center`}>
+        <FontAwesomeIcon icon={type === 'in'?faArrowDownLong:faArrowUpLong} className="w-full h-full rotate-45"/>
+      </div>
+      <div className="w-full">
+        <p className="text-projectPallet-tertiary">Total {type === 'in'?'Income':'Outcome'}</p>
+        <div className="flex justify-between">
+          <h2 className="font-bold text-2xl">R$ {value}</h2>
+          <div className={`${severity === 'success'?'bg-green-700':'bg-red-700'} py-1 px-4 flex items-center text-sm rounded-2xl bg-opacity-20 ${severity === 'success'?'text-green-700':'text-red-700'}`}>
+            <p>{porcentage}%</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default InOutComes
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,15 +1,19 @@
+import InOutComes from "@/components/InOutComes"
 import Layout from "@/components/Layout"
 import Container from '@components/Container'
+import { faArrowDownLong, faArrowUpLong } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const DashboardPage = () => {
 	return (
 		<Layout className="pt-10 flex gap-6">
 			<div className="flex flex-col w-full gap-6">
 				<div className="flex w-full gap-6">
-					<Container>
+					<Container className="flex gap-5">
+						<InOutComes type='in' value='0,00' porcentage='0' severity='success'/>
 					</Container>
-					<Container>
-					
+					<Container className="flex gap-5">
+						<InOutComes type='out' value='0,00' porcentage='0' severity='danger'/>
 					</Container>
 				</div>
 				<div className="flex flex-col gap-6">
