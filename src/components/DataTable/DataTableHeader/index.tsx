f19a3dff6cc1dcbb4ca36b2e281dd7484fabe1e9
IsmaelSantiagoo <ismaelfreitas.santiago@gmail.com>
2024-10-17 09:22:54 -0300

    Fix: Bug em actions do componente "DataTable" corrigido

--- src/components/DataTable/DataTableHeader/index.tsx
+++ src/components/DataTable/DataTableHeader/index.tsx
@@ -2,7 +2,7 @@ import { Button } from "@mui/material"
 import { DataTableHeaderProps } from "./types"
 import DatePicker from "@/components/DatePicker"
 
-const DataTableHeader = ({ title, selectedRows, showActions, onAddAction, onEditAction, onDeleteAction, controls, onDataChange, dataInicio }: DataTableHeaderProps) => {
+const DataTableHeader = ({ title, selectedRows, showActions, onAddAction, onEditAction, onDeleteAction, controls = 'visible', onDataChange, dataInicio }: DataTableHeaderProps) => {
 
   return (
     <div className="p-3 bg-projectPallet-quaternary flex items-center rounded-t-xl gap-2">
@@ -13,18 +13,18 @@ const DataTableHeader = ({ title, selectedRows, showActions, onAddAction, onEdit
           }
         </h1>
       </div>
-      <div className="flex gap-2">
+      <div className="w-full flex items-center gap-2 justify-end" style={{ display: controls ? 'flex' : 'none'}}>
         <DatePicker dataInicio={dataInicio} onChange={onDataChange}/>
         {
-          !showActions && controls === 'visible' &&
+          !showActions && controls === 'visible' && selectedRows.length === 0 &&
             <Button className="text-white bg-projectPallet-secondary px-5 font-bold min-w-32 max-w-32 h-10 rounded-lg" onClick={() => onAddAction()}>ADICIONAR</Button>
         }
-      </div>
-      <div className="w-full flex items-center gap-2 justify-end" style={{ display: showActions ? 'flex' : 'none'}}>
         {
-          selectedRows.length === 1 && <Button className="text-white px-5 font-bold min-w-32 max-w-32 border-2 border-projectPallet-secondary h-10 rounded-lg" variant="outlined" onClick={() => onEditAction()}>EDITAR</Button>
+          showActions && selectedRows.length === 1 && <Button className="text-white px-5 font-bold min-w-32 max-w-32 border-2 border-projectPallet-secondary h-10 rounded-lg" variant="outlined" onClick={() => onEditAction()}>EDITAR</Button>
         }
-        <Button className="text-white px-5 font-bold min-w-32 max-w-32 border-2 border-projectPallet-secondary h-10 rounded-lg" variant="outlined" onClick={() => onDeleteAction()}>REMOVER</Button>
+        {
+          showActions && selectedRows.length > 0 && <Button className="text-white px-5 font-bold min-w-32 max-w-32 border-2 border-projectPallet-secondary h-10 rounded-lg" variant="outlined" onClick={() => onDeleteAction()}>REMOVER</Button>
+        }
       </div>
     </div>
   )
