1d9b44dbbb6b7331efbbb0375b91d6c8aca018cd
IsmaelSantiagoo <ismaelfreitas.santiago@gmail.com>
2024-10-23 17:01:15 -0300

    Corrigindo visualização de item sem url

--- src/components/Selector/index.tsx
+++ src/components/Selector/index.tsx
@@ -28,7 +28,7 @@ const Selector = ({ items, label, defaultValue, onChange, showBrand = false }: S
 				<div className="flex gap-2 w-full items-center justify-between bg-projectPallet-secondary hover:bg-projectPallet-tertiary cursor-pointer rounded-xl p-2" onClick={() => setActive(!active)}>
 					<div className="flex gap-2 items-center">
 						{
-							value && showBrand && <img className="rounded-full w-10 h-10 bg-white" src={value.url ? `https://cdn.brandfetch.io/${value.url}/w/400/h/400`: 'https://www.advocacianunes.com.br/wp-content/uploads/2022/04/logo-pix-icone-1024.png'}/>
+							value && value.url !== '-' && showBrand && <img className="rounded-full w-10 h-10 bg-white" src={value.url ? `https://cdn.brandfetch.io/${value.url}/w/400/h/400`: 'https://www.advocacianunes.com.br/wp-content/uploads/2022/04/logo-pix-icone-1024.png'}/>
 						}
 						<span className="text-md font-bold p-2">{value ? value.label : 'Escolha uma opção'}</span>
 					</div>
@@ -42,7 +42,7 @@ const Selector = ({ items, label, defaultValue, onChange, showBrand = false }: S
 								items.map( ({ id, url, label }) => (
 									<div key={label} className="flex gap-2 w-full items-center hover:bg-projectPallet-tertiary cursor-pointer rounded-xl p-2" onClick={() => {setValue({ id: id, url: url, label: label});setActive(false)}}>
 										{
-											value && showBrand && <img className="rounded-full w-10 h-10 bg-white" src={url ? `https://cdn.brandfetch.io/${url}/w/400/h/400`: 'https://www.advocacianunes.com.br/wp-content/uploads/2022/04/logo-pix-icone-1024.png'} alt={label}/>
+											value && url !== '-' && showBrand && <img className="rounded-full w-10 h-10 bg-white" src={url ? `https://cdn.brandfetch.io/${url}/w/400/h/400`: 'https://www.advocacianunes.com.br/wp-content/uploads/2022/04/logo-pix-icone-1024.png'} alt={label}/>
 										}
 										<span className="text-md font-bold">{label}</span>
 									</div>
