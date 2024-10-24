53d93ae2bd23e0c565a5fa53de5c164cf79b6264
IsmaelSantiagoo <ismaelfreitas.santiago@gmail.com>
2024-10-24 14:42:28 -0300

    Suporte a números negativos adicionado

--- src/components/InputBRL/index.tsx
+++ src/components/InputBRL/index.tsx
@@ -2,34 +2,51 @@ import React, { useEffect, useState } from 'react';
 
 const InputBRL: React.FC<InputBRLProps> = ({ value, label, placeholder, onChange }) => {
   const [inputValue, setInputValue] = useState<string>(value);
+  const [isNegative, setIsNegative] = useState<boolean>(false)
 
   // Atualiza o estado local sempre que o valor recebido muda
   useEffect(() => {
     setInputValue(value);
+    if (value.includes('-')) setIsNegative(true)
+      console.log(value)
   }, [value]);
 
   const formatToBRL = (value: string) => {
     const numericValue = value.replace(/\D/g, '')
-    if (numericValue) {
+
+    if (numericValue && numericValue !== '00') {
+
       // Converte a string numérica em um número e formata
-      const formattedValue = (Number(numericValue) / 100).toLocaleString('pt-BR', {
+      const formattedValue = isNegative ? parseFloat(((Number(numericValue) / 100) * -1).toFixed(2)) : ((Number(numericValue) / 100))
+
+      return formattedValue.toLocaleString('pt-BR', {
         style: 'currency',
         currency: 'BRL',
       });
-      return formattedValue;
     }
-    return '';
+
+    return (0).toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    });
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = e.target.value;
+
+    if (newValue.includes('-')) {
+      setIsNegative(true)
+    } else {
+      setIsNegative(false)
+    }
     
     // Atualiza o inputValue com o novo valor
     setInputValue(newValue);
     
     // Passa o valor numérico não formatado (apenas dígitos) para o onChange
     const numericValue = newValue.replace(/\D/g, '').replace(/(\d{1,})(\d{2})$/, '$1.$2')
-    onChange(numericValue);
+
+    onChange( newValue.includes('-') ? (parseFloat(numericValue) * -1).toString() : numericValue);
   };
 
   return (
