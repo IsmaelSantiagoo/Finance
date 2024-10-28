883b9a23a2c6d6ec12d8ec6b7e918b74293c0702
IsmaelSantiagoo <ismaelfreitas.santiago@gmail.com>
2024-10-28 15:06:14 -0300

    Alterando cor padrão do Container

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -2,7 +2,7 @@ import { ContainerProps } from "./types"
 
 const Container = ({children, className}: ContainerProps) => {
   return (
-    <div className={`w-full rounded-xl bg-projectPalletLight-secondary dark:bg-projectPallet-quaternary ${className}`}>
+    <div className={`w-full rounded-xl bg-projectPalletLight-quaternary dark:bg-projectPallet-quaternary ${className}`}>
       {children}
     </div>
   )
