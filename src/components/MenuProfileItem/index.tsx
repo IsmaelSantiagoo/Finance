4cabef897aeaf7e8de58cbdeaa8d8adc39951269
IsmaelSantiagoo <ismaelfreitas.santiago@gmail.com>
2024-10-28 15:02:40 -0300

    Corrigindo tema ao clicar no link do perfil

--- src/components/MenuProfileItem/index.tsx
+++ src/components/MenuProfileItem/index.tsx
@@ -4,12 +4,12 @@ import Link from 'next/link'
 const MenuProfileItem = ({img, username, active}: MenuProfileItemProps) => {
   return (
     <Link href={'/profile'}>
-      <div className={`flex items-center justify-between rounded-xl p-3 ${active && 'bg-projectPallet-secondary'}`}>
+      <div className={`flex items-center justify-between rounded-xl p-3 ${active && 'dark:bg-projectPallet-secondary bg-white' }`}>
         <div className="flex gap-2 items-center">
           <Image src={img} alt="foto de perfil" width={50}/>
           <div className="font-bold max-w-[130px]">
-            <h2 className='text-md truncate'>{username}</h2>
-            <p className="text-sm font-light text-zinc-300 truncate">Meu perfil</p>
+            <h2 className={`text-md truncate ${active && 'text-projectPallet-secondary dark:text-white'}`}>{username}</h2>
+            <p className={`text-sm font-light ${active && 'text-projectPallet-secondary dark:text-white'} truncate`}>Meu perfil</p>
           </div>
         </div>
       </div>
